7d2692f1b2aceae45c0b2fcbbd51f1b9da1fa249
Sapan-Ravidas <sapanravidas@gmail.com>
2022-02-03 20:24:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+from gettext import find
+from itertools import count
+from socket import NI_DGRAM
+from sre_constants import BRANCH
+from utils import Util
+import utils
+
+import sys
+import nltk
+
+FILE_MATCHES = 1
+SENTENCE_MATCHES = 1
+
+
+def find_matches(query, file_words, file_idfs):
+    # Determine top file matches according to TF-IDF
+    try:
+        filenames = util.top_files(query, file_words, file_idfs, n=FILE_MATCHES) 
+
+        # Extract Sentences from top files
+        sentences = dict()
+        for filename in filenames:
+            for passage in files[filename].split("\n"):
+                for sentence in nltk.sent_tokenize(passage):
+                    tokens = Util.tokenize(sentence)
+                    if tokens:
+                        sentences[sentence] = tokens
+    
+        # Compute IDF-values accross sentences
+        idfs = Util.compute_idfs(sentences)
+
+        # Determine top sentence matches
+        matches = util.top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
+        if not matches:
+            return {'Couldn\'t find the answer'}
+        return matches
+    except:
+        return {'Couldn\'t find the answer...'}
+
+
+
+def print_matches(matches):
+    """
+    Print all the matching sentences for query
+    """
+    for match in matches:
+        print(match)
+
+
+
+def writeToFile(query, ans_file, counter, matches):
+    """
+    Write the matching sentences in a file for output
+    """
+    ans_file.writelines(f"{counter}. {query}")
+    for match in matches:
+        ans_file.writelines(match)
+    
+    ans_file.writelines("\n")
+    ans_file.writelines("\n")
+
+
+
+if __name__ == '__main__':
+    # check command-line arguments
+    util = Util()
+    N_ARGS = len(sys.argv)
+    
+    if N_ARGS not in  {2, 3}:
+        sys.exit("Usage: python main.py corpus")
+
+    # Mapping of filename : content
+    files = Util.load_files(sys.argv[1])
+
+    file_words = {
+        filename : Util.tokenize(files[filename]) for filename in files
+    }
+
+    # Calculate the IDF values accrosss the files
+    file_idfs = Util.compute_idfs(file_words)
+    
+    if N_ARGS == 2:
+
+        CHOICE = "Y"
+        while CHOICE.lower() in {'y', 'yes'}:
+            # Prompt user for query
+            query = set(Util.tokenize(input("Query: ")))
+            matches = find_matches(query, file_words, file_idfs)
+
+            print_matches(matches)
+            print("------>")
+            CHOICE = input("Want to continue? yes/no : ")
+    
+    elif N_ARGS == 3:
+        q_file = sys.argv[2]
+        with open(q_file) as questions:
+            counter = 1
+            with open(f"ans_{q_file}", "w") as answers:
+                while True:
+                    Q = questions.readline()
+                    if Q == "" or Q == None: break
+
+                    matches = find_matches(set(Util.tokenize(Q)), file_words, file_idfs)
+                    writeToFile(Q, answers, counter, matches)
+                    counter += 1
+                    
---
+++ utils.py
@@ -0,0 +1,110 @@
+import nltk
+import sys
+import os
+import string
+import math
+
+class Util:
+
+    @classmethod
+    def load_files(cls, directory):
+        """
+        Given a directory name, return a dictionary mapping the filename of each file
+        `.txt` file inside that directory to the file's contents as a string 
+        """
+        dictionry = {}
+    
+        for file in os.listdir(directory):
+            with open(os.path.join(directory, file), encoding="utf-8") as ofile:
+                dictionry[file] = ofile.read()
+    
+        return dictionry
+
+
+    @classmethod
+    def tokenize(cls, document):
+        """
+        Given a document (represented as a string), return a list of all of the
+        words in that document, in order.
+
+        Process document by coverting all words to lowercase, and removing any
+        punctuation or English stopwords.
+        """
+        tokenized = nltk.tokenize.word_tokenize(document.lower())
+        final_list = [x for x in tokenized if x not in string.punctuation and x not in nltk.corpus.stopwords.words("english")]
+        return final_list
+
+
+
+    @classmethod
+    def compute_idfs(cls, documents):
+        """
+        Given a dictonary of 'documents' that maps names of documents to a list
+        of words, return a dictionary that maps words to their IDF values
+
+        Any word that appears in at-least one of the documents  should be in the
+        resultig dictionary.
+        """
+        idf_dictio = {}
+        doc_len = len(documents)
+
+        unique_words = set(sum(documents.values(), []))
+    
+        for word in unique_words:
+            count = 0
+            for doc in documents.values():
+                if word in doc:
+                    count += 1
+        
+            idf_dictio[word] = math.log(doc_len / count)
+    
+        return idf_dictio
+
+
+
+    def top_files(self, query, file_words, idfs, n):
+        """
+        Given a 'query' (a set of words), 'files_words' (a dictinary mapping names
+        of files to a list of their words), and 'idfs' (a dictionary mapping words
+        to their IDF value), returns a list of the filenames of the 'n' top files 
+        that matches the query, ranked according to tf-idf
+        """
+
+        scores = {}
+        for filename, file_content in file_words.items():
+            file_score = 0
+            for word in query:
+                if word in file_content:
+                    # file_score += file_content.count(word) * idfs[word]
+                    file_score += ((file_content.count(word) / len(file_content)) * idfs[word])
+
+            if file_score != 0:
+                scores[filename] = file_score
+
+        sorted_by_score = [k for k, v in sorted(scores.items(), key=lambda x : x[1], reverse=True)]
+        return sorted_by_score[:n]
+
+
+
+    def top_sentences(self, query, sentences, idfs, n):
+        """
+        Given a 'query (a set of words), 'sentences' (a dictionary mapping sentecnes
+        to a list of their words), nd 'idfs' (a dictionary mapping words to their IDF
+        values), return a list of the 'n'n top sentences that match the query, ranked
+        according to IDF. If there are ties, preference should be given to sentence 
+        that have a hiher query density. 
+        """
+        scores = {}
+        for sentence, sentwords in sentences.items():
+            score = 0
+            for word in query:
+                if word in sentwords:
+                    score += idfs[word]
+        
+            if score != 0:
+                density = sum([sentwords.count(x) for x in query]) / len(sentwords)
+                scores[sentence] = (score, density)
+    
+        sorted_by_score = [k for k, v in sorted(scores.items(), key = lambda x : (x[1][0], x[1][1]), reverse=True)]
+        return sorted_by_score[:n]
+
